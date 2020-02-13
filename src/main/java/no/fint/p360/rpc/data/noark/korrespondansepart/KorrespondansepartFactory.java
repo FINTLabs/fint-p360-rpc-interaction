5473bf03fe819eaa84696d45bbe5e27dd2286155
OlaStalberg <olastaalberg@gmail.com>
2020-02-13 09:35:25 +0100

    added KorrespondansepartService and KorrespondansepartFactory

---
+++ src/main/java/no/fint/p360/rpc/data/noark/korrespondansepart/KorrespondansepartFactory.java
@@ -0,0 +1,146 @@
+package no.fint.p360.rpc.data.noark.korrespondansepart;
+
+import no.fint.model.felles.kompleksedatatyper.Kontaktinformasjon;
+import no.fint.model.felles.kompleksedatatyper.Personnavn;
+import no.fint.model.resource.administrasjon.arkiv.KorrespondansepartResource;
+import no.fint.model.resource.felles.kompleksedatatyper.AdresseResource;
+import no.fint.p360.rpc.data.utilities.FintUtils;
+import no.p360.model.ContactService.*;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import static java.util.Optional.ofNullable;
+import static no.fint.p360.rpc.data.utilities.FintUtils.*;
+
+@SuppressWarnings("Duplicates")
+@Service
+public class KorrespondansepartFactory {
+
+    public KorrespondansepartResource toFintResource(PrivatePerson result) {
+
+        if (result == null) {
+            return null;
+        }
+
+        KorrespondansepartResource korrespondansepartResource = new KorrespondansepartResource();
+        korrespondansepartResource.setAdresse(FintUtils.createAdresse(result));
+        korrespondansepartResource.setKontaktinformasjon(FintUtils.createKontaktinformasjon(result));
+        korrespondansepartResource.setKorrespondansepartNavn(FintUtils.getFullNameString(result));
+        korrespondansepartResource.setSystemId(createIdentifikator(result.getRecno().toString()));
+        optionalValue(result.getPersonalIdNumber())
+                .filter(StringUtils::isNotBlank)
+                .map(FintUtils::createIdentifikator)
+                .ifPresent(korrespondansepartResource::setFodselsnummer);
+
+        return korrespondansepartResource;
+    }
+
+    public KorrespondansepartResource toFintResource(ContactPerson result) {
+
+        if (result == null) {
+            return null;
+        }
+
+        KorrespondansepartResource korrespondansepartResource = new KorrespondansepartResource();
+        korrespondansepartResource.setAdresse(FintUtils.createAdresse(result));
+        korrespondansepartResource.setKontaktinformasjon(FintUtils.createKontaktinformasjon(result));
+        korrespondansepartResource.setKorrespondansepartNavn(FintUtils.getFullNameString(result));
+        korrespondansepartResource.setSystemId(createIdentifikator(result.getRecno().toString()));
+
+        return korrespondansepartResource;
+    }
+
+    public KorrespondansepartResource toFintResource(Enterprise result) {
+
+        if (result == null) {
+            return null;
+        }
+
+        KorrespondansepartResource korrespondansepartResource = new KorrespondansepartResource();
+        korrespondansepartResource.setAdresse(FintUtils.createAdresse(result));
+        korrespondansepartResource.setKontaktinformasjon(FintUtils.createKontaktinformasjon(result));
+        korrespondansepartResource.setKorrespondansepartNavn(result.getName());
+        korrespondansepartResource.setKontaktperson(FintUtils.getKontaktpersonString(result));
+        korrespondansepartResource.setSystemId(createIdentifikator(result.getRecno().toString()));
+        optionalValue(result.getEnterpriseNumber())
+                .filter(StringUtils::isNotBlank)
+                .map(FintUtils::createIdentifikator)
+                .ifPresent(korrespondansepartResource::setOrganisasjonsnummer);
+
+        return korrespondansepartResource;
+    }
+
+    public SynchronizePrivatePersonArgs toPrivatePerson(KorrespondansepartResource korrespondansepartResource) {
+        SynchronizePrivatePersonArgs synchronizePrivatePersonArgs = new SynchronizePrivatePersonArgs();
+        Parameter__5 parameter = new Parameter__5();
+        Personnavn personnavn = parsePersonnavn(korrespondansepartResource.getKorrespondansepartNavn());
+        parameter.setFirstName(personnavn.getFornavn());
+        parameter.setLastName(personnavn.getEtternavn());
+        parameter.setPersonalIdNumber(
+                        korrespondansepartResource.getFodselsnummer().getIdentifikatorverdi());
+
+        ofNullable(korrespondansepartResource.getKontaktinformasjon())
+                .map(Kontaktinformasjon::getEpostadresse)
+                .ifPresent(parameter::setEmail);
+
+        ofNullable(korrespondansepartResource.getKontaktinformasjon())
+                .map(Kontaktinformasjon::getMobiltelefonnummer)
+                .ifPresent(parameter::setMobilePhone);
+
+        parameter.setPrivateAddress(createAddress(korrespondansepartResource.getAdresse(), parameter.getPrivateAddress()));
+
+        synchronizePrivatePersonArgs.setParameter(parameter);
+        return synchronizePrivatePersonArgs;
+    }
+
+    public SynchronizeEnterpriseArgs toEnterprise(KorrespondansepartResource korrespondansepartResource) {
+        SynchronizeEnterpriseArgs synchronizeEnterpriseArgs = new SynchronizeEnterpriseArgs();
+        Parameter__4 parameter = new Parameter__4();
+
+        parameter.setName(korrespondansepartResource.getKorrespondansepartNavn());
+        parameter.setEnterpriseNumber(korrespondansepartResource.getOrganisasjonsnummer().getIdentifikatorverdi());
+
+        ofNullable(korrespondansepartResource.getKontaktinformasjon())
+                .map(Kontaktinformasjon::getEpostadresse)
+                .ifPresent(parameter::setEmail);
+
+        ofNullable(korrespondansepartResource.getKontaktinformasjon())
+                .map(Kontaktinformasjon::getMobiltelefonnummer)
+                .ifPresent(parameter::setMobilePhone);
+
+        ofNullable(korrespondansepartResource.getKontaktinformasjon())
+                .map(Kontaktinformasjon::getTelefonnummer)
+                .ifPresent(parameter::setPhoneNumber);
+
+        ofNullable(korrespondansepartResource.getKontaktinformasjon())
+                .map(Kontaktinformasjon::getNettsted)
+                .ifPresent(parameter::setWeb);
+
+        parameter.setPostAddress(createAddress(korrespondansepartResource.getAdresse(), parameter.getPostAddress()));
+
+        synchronizeEnterpriseArgs.setParameter(parameter);
+        return synchronizeEnterpriseArgs;
+    }
+
+    private PrivateAddress__3 createAddress(AdresseResource adresse, PrivateAddress__3 address ) {
+        address.setCountry("NOR");
+        ofNullable(adresse.getAdresselinje())
+                .map(l -> l.get(0))
+                .ifPresent(address::setStreetAddress);
+        address.setZipCode(adresse.getPostnummer());
+        address.setZipPlace(adresse.getPoststed());
+
+        return address;
+    }
+
+    private PostAddress__4 createAddress(AdresseResource adresse, PostAddress__4 address) {
+        address.setCountry("NOR");
+        ofNullable(adresse.getAdresselinje())
+                .map(l -> l.get(0))
+                .ifPresent(address::setStreetAddress);
+        address.setZipCode(adresse.getPostnummer());
+        address.setZipPlace(adresse.getPoststed());
+
+        return address;
+    }
+}
---
+++ src/main/java/no/fint/p360/rpc/data/noark/korrespondansepart/KorrespondansepartService.java
@@ -0,0 +1,94 @@
+package no.fint.p360.rpc.data.noark.korrespondansepart;
+
+import lombok.extern.slf4j.Slf4j;
+import no.fint.model.resource.administrasjon.arkiv.KorrespondansepartResource;
+import no.fint.p360.data.exception.*;
+import no.fint.p360.rpc.p360Service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import static no.fint.p360.rpc.data.utilities.FintUtils.validIdentifikator;
+
+
+@Slf4j
+@Service
+public class KorrespondansepartService {
+
+    @Autowired
+    private KorrespondansepartFactory korrespondansepartFactory;
+
+    @Autowired
+    private ContactService contactService;
+
+    public KorrespondansepartResource getKorrespondansepartBySystemId(int id) throws KorrespondansepartNotFound {
+
+        Supplier<KorrespondansepartResource> enterpriseContact = () ->
+                korrespondansepartFactory.toFintResource(contactService.getEnterpriseByRecno(id));
+        Supplier<KorrespondansepartResource> privateContact = () ->
+                korrespondansepartFactory.toFintResource(contactService.getPrivatePersonByRecno(id));
+        Supplier<KorrespondansepartResource> contact = () ->
+                korrespondansepartFactory.toFintResource(contactService.getContactPersonByRecno(id));
+
+        return Stream.of(enterpriseContact, privateContact, contact)
+                .parallel()
+                .map(Supplier::get)
+                .filter(Objects::nonNull)
+                .findAny()
+                .orElseThrow(() -> new KorrespondansepartNotFound("Recno " + id + " not found"));
+
+    }
+
+    public KorrespondansepartResource getKorrespondansepartByFodselsnummer(String fodselsnummer) throws KorrespondansepartNotFound {
+        try {
+            return  korrespondansepartFactory.toFintResource(
+                    contactService.getPrivatePersonByPersonalIdNumber(fodselsnummer)
+            );
+        } catch (PrivatePersonNotFound e) {
+            throw new KorrespondansepartNotFound(e.getMessage());
+        }
+
+    }
+
+    public KorrespondansepartResource getKorrespondansepartByOrganisasjonsnummer(String organisasjonsNummer) throws KorrespondansepartNotFound {
+        try {
+            return korrespondansepartFactory.toFintResource(
+                    contactService.getEnterpriseByEnterpriseNumber(organisasjonsNummer));
+        } catch (EnterpriseNotFound e) {
+            throw new KorrespondansepartNotFound(e.getMessage());
+        }
+    }
+
+    public Stream<KorrespondansepartResource> search(Map<String, String> queryParams) {
+        Supplier<Stream<KorrespondansepartResource>> enterpriseContacts = () ->
+                contactService.searchEnterprise(queryParams).map(korrespondansepartFactory::toFintResource);
+        Supplier<Stream<KorrespondansepartResource>> privateContacts = () ->
+                contactService.searchPrivatePerson(queryParams).map(korrespondansepartFactory::toFintResource);
+        Supplier<Stream<KorrespondansepartResource>> contacts = () ->
+                contactService.searchContactPerson(queryParams).map(korrespondansepartFactory::toFintResource);
+
+        return Stream.of(enterpriseContacts, privateContacts, contacts)
+                .parallel()
+                .flatMap(Supplier::get);
+    }
+
+    public KorrespondansepartResource createKorrespondansepart(KorrespondansepartResource korrespondansepartResource) throws CreateContactException, CreateEnterpriseException {
+        if (validIdentifikator(korrespondansepartResource.getFodselsnummer())) {
+            int recNo = contactService.createPrivatePerson(korrespondansepartFactory.toPrivatePerson(korrespondansepartResource));
+            return  korrespondansepartFactory.toFintResource(contactService.getPrivatePersonByRecno(recNo));
+        } else if (validIdentifikator(korrespondansepartResource.getOrganisasjonsnummer())) {
+            int recNo = contactService.createEnterprise(korrespondansepartFactory.toEnterprise(korrespondansepartResource));
+            return korrespondansepartFactory.toFintResource(contactService.getEnterpriseByRecno(recNo));
+        } else {
+            throw new IllegalArgumentException("Invalid Korrespondansepart - neither fodselsnummer nor organisasjonsnummer is set.");
+        }
+    }
+
+    //public boolean health() {
+     //   return  contactService.ping();
+    //}
+}
