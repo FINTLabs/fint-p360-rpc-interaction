abf53effdef11d3b5e7289836d2ba72384178da4
Jørgen Tellnes <jorgen@arkitektum.no>
2020-02-13 16:28:36 +0100

    Add RPC-adjusted TilskuddFartoy classes

---
+++ src/main/java/no/fint/p360/rpc/data/kulturminne/TilskuddFartoyFactory.java
@@ -0,0 +1,209 @@
+package no.fint.p360.rpc.data.kulturminne;
+
+import com.google.common.base.Strings;
+import lombok.extern.slf4j.Slf4j;
+import no.fint.model.kultur.kulturminnevern.TilskuddFartoy;
+import no.fint.model.resource.Link;
+import no.fint.model.resource.administrasjon.arkiv.JournalpostResource;
+import no.fint.model.resource.administrasjon.arkiv.MerknadResource;
+import no.fint.model.resource.administrasjon.arkiv.PartsinformasjonResource;
+import no.fint.model.resource.kultur.kulturminnevern.TilskuddFartoyResource;
+import no.fint.p360.data.exception.GetDocumentException;
+import no.fint.p360.data.exception.IllegalCaseNumberFormat;
+import no.fint.p360.data.exception.NoSuchTitleDimension;
+import no.fint.p360.data.exception.UnableToParseTitle;
+import no.fint.p360.rpc.data.noark.common.NoarkFactory;
+import no.fint.p360.rpc.data.noark.journalpost.JournalpostFactory;
+import no.fint.p360.rpc.data.noark.korrespondansepart.KorrespondansepartFactory;
+import no.fint.p360.rpc.data.utilities.FintUtils;
+import no.fint.p360.rpc.data.utilities.NOARKUtils;
+import no.fint.p360.rpc.data.utilities.P360Utils;
+import no.fint.p360.rpc.data.utilities.TitleParser;
+import no.p360.model.CaseService.*;
+import no.p360.model.DocumentService.CreateDocumentArgs;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+import static no.fint.p360.rpc.data.utilities.P360Utils.applyParameterFromLink;
+
+@Slf4j
+@Service
+public class TilskuddFartoyFactory {
+
+    @Autowired
+    private NoarkFactory noarkFactory;
+
+    @Autowired
+    private KorrespondansepartFactory korrespondansepartFactory;
+
+    @Autowired
+    private JournalpostFactory journalpostFactory;
+
+    @Autowired
+    private TilskuddFartoyDefaults tilskuddFartoyDefaults;
+
+    public TilskuddFartoyResource toFintResource(Case caseResult) throws GetDocumentException, IllegalCaseNumberFormat {
+
+        TilskuddFartoyResource tilskuddFartoy = new TilskuddFartoyResource();
+        String caseNumber = caseResult.getCaseNumber();
+
+        String caseYear = NOARKUtils.getCaseYear(caseNumber);
+        String sequenceNumber = NOARKUtils.getCaseSequenceNumber(caseNumber);
+
+        try {
+            TitleParser.Title title = TitleParser.parseTitle(caseResult.getTitle());
+            tilskuddFartoy.setFartoyNavn(Strings.nullToEmpty(title.getDimension(TitleParser.FARTOY_NAVN)));
+            tilskuddFartoy.setKallesignal(Strings.nullToEmpty(title.getDimension(TitleParser.FARTOY_KALLESIGNAL)));
+            tilskuddFartoy.setKulturminneId(Strings.nullToEmpty(title.getDimension(TitleParser.KULTURMINNE_ID)));
+            tilskuddFartoy.setSoknadsnummer(FintUtils.createIdentifikator(caseResult.getExternalId().getId()));
+        } catch (UnableToParseTitle | NoSuchTitleDimension e) {
+            log.error("{}", e.getMessage(), e);
+        }
+
+        noarkFactory.getSaksmappe(caseResult, tilskuddFartoy);
+
+        tilskuddFartoy.addSelf(Link.with(TilskuddFartoy.class, "mappeid", caseYear, sequenceNumber));
+        tilskuddFartoy.addSelf(Link.with(TilskuddFartoy.class, "systemid", caseResult.getRecno().toString()));
+        tilskuddFartoy.addSelf(Link.with(TilskuddFartoy.class, "soknadsnummer", caseResult.getExternalId().getId()));
+
+        return tilskuddFartoy;
+    }
+
+
+    public List<TilskuddFartoyResource> toFintResourceList(List<Case> caseResults) throws GetDocumentException, IllegalCaseNumberFormat {
+        List<TilskuddFartoyResource> result = new ArrayList<>(caseResults.size());
+        for (Case caseResult : caseResults) {
+            result.add(toFintResource(caseResult));
+        }
+        return result;
+    }
+
+    public CreateCaseArgs convertToCreateCase(TilskuddFartoyResource tilskuddFartoy) {
+
+        CreateCaseArgs createCaseArgs = new CreateCaseArgs();
+
+        Parameter parameter = new Parameter();
+
+        tilskuddFartoyDefaults.applyDefaultsToCreateCaseParameter(parameter);
+
+        parameter.setTitle(TitleParser.getTitleString(tilskuddFartoy));
+        parameter.setExternalId(P360Utils.getExternalIdParameter(tilskuddFartoy.getSoknadsnummer()));
+
+        applyParameterFromLink(
+                tilskuddFartoy.getAdministrativEnhet(),
+                s -> parameter.setResponsibleEnterpriseRecno(Integer.valueOf(s))
+        );
+
+        applyParameterFromLink(
+                tilskuddFartoy.getArkivdel(),
+                parameter::setSubArchive
+        );
+
+        applyParameterFromLink(
+                tilskuddFartoy.getSaksstatus(),
+                parameter::setStatus
+        );
+
+        if (tilskuddFartoy.getSkjerming() != null) {
+            applyParameterFromLink(
+                    tilskuddFartoy.getSkjerming().getTilgangsrestriksjon(),
+                    parameter::setAccessCode);
+
+            applyParameterFromLink(
+                    tilskuddFartoy.getSkjerming().getSkjermingshjemmel(),
+                    parameter::setParagraph);
+
+            // TODO createCaseParameter.setAccessGroup();
+        }
+
+        // TODO createCaseParameter.setCategory(objectFactory.createCaseParameterBaseCategory("recno:99999"));
+        // TODO Missing parameters
+        //createCaseParameter.setRemarks();
+        //createCaseParameter.setStartDate();
+        //createCaseParameter.setUnofficialTitle();
+
+
+
+        List<Contact> contacts = new ArrayList<>();
+
+        tilskuddFartoy
+                .getPart()
+                .stream()
+                .map(this::createCaseContactParameter)
+                .forEach(contacts::add);
+
+        parameter.setContacts(contacts);
+
+        List<Remark> remarks = new ArrayList<>();
+        if (tilskuddFartoy.getMerknad() != null) {
+            tilskuddFartoy
+                    .getMerknad()
+                    .stream()
+                    .map(this::createCaseRemarkParameter)
+                    .forEach(remarks::add);
+        }
+        parameter.setRemarks(remarks);
+
+        // TODO Responsible person
+        /*
+        createCaseParameter.setResponsiblePersonIdNumber(
+                objectFactory.createCaseParameterBaseResponsiblePersonIdNumber(
+                        tilskuddFartoy.getSaksansvarlig().get(0).getHref()
+                )
+        );
+        */
+        createCaseArgs.setParameter(parameter);
+
+        return createCaseArgs;
+    }
+
+    private Remark createCaseRemarkParameter(MerknadResource merknadResource) {
+        Remark remark = new Remark();
+        remark.setContent(merknadResource.getMerknadstekst());
+
+        merknadResource
+                .getMerknadstype()
+                .stream()
+                .map(Link::getHref)
+                .filter(StringUtils::isNotBlank)
+                .map(s -> StringUtils.substringAfterLast(s, "/"))
+                .findFirst()
+                .ifPresent(remark::setRemarkType);
+
+        return remark;
+    }
+
+
+    public Contact createCaseContactParameter(PartsinformasjonResource partsinformasjon) {
+        Contact contact = new Contact();
+
+        partsinformasjon
+                .getPart()
+                .stream()
+                .map(Link::getHref)
+                .filter(StringUtils::isNotBlank)
+                .map(s -> StringUtils.substringAfterLast(s, "/"))
+                .findFirst()
+                .ifPresent(contact::setReferenceNumber);
+
+        partsinformasjon
+                .getPartRolle()
+                .stream()
+                .map(Link::getHref)
+                .filter(StringUtils::isNotBlank)
+                .map(s -> StringUtils.substringAfterLast(s, "/"))
+                .findFirst()
+                .ifPresent(contact::setRole);
+
+        return contact;
+    }
+
+    public CreateDocumentArgs convertToCreateDocument(JournalpostResource journalpostResource, String caseNumber) {
+        return journalpostFactory.toP360(journalpostResource, caseNumber);
+    }
+}
---
+++ src/main/java/no/fint/p360/rpc/data/kulturminne/TilskuddfartoyService.java
@@ -0,0 +1,112 @@
+package no.fint.p360.rpc.data.kulturminne;
+
+import lombok.extern.slf4j.Slf4j;
+import no.fint.model.resource.administrasjon.arkiv.JournalpostResource;
+import no.fint.model.resource.kultur.kulturminnevern.TilskuddFartoyResource;
+import no.fint.p360.data.exception.*;
+import no.fint.p360.rpc.data.utilities.Constants;
+import no.fint.p360.rpc.data.utilities.FintUtils;
+import no.fint.p360.rpc.p360Service.CaseService;
+import no.fint.p360.rpc.p360Service.DocumentService;
+import no.p360.model.CaseService.Case;
+import no.p360.model.DocumentService.CreateDocumentArgs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class TilskuddfartoyService {
+
+    @Autowired
+    private CaseService caseService;
+
+    @Autowired
+    private DocumentService documentService;
+
+    @Autowired
+    private TilskuddFartoyFactory tilskuddFartoyFactory;
+
+    public TilskuddFartoyResource createTilskuddFartoyCase(TilskuddFartoyResource tilskuddFartoy) throws NotTilskuddfartoyException, CreateCaseException, GetTilskuddFartoyNotFoundException, GetTilskuddFartoyException, CreateDocumentException, GetDocumentException, IllegalCaseNumberFormat {
+
+        String caseNumber = caseService.createCase(tilskuddFartoyFactory.convertToCreateCase(tilskuddFartoy));
+
+        for (JournalpostResource journalpostResource : tilskuddFartoy.getJournalpost()) {
+            CreateDocumentArgs tilskuddFartoyDocument = tilskuddFartoyFactory.convertToCreateDocument(journalpostResource, caseNumber);
+            documentService.createDocument(tilskuddFartoyDocument);
+        }
+        return getTilskuddFartoyCaseByCaseNumber(caseNumber);
+    }
+
+    public TilskuddFartoyResource updateTilskuddFartoyCase(String caseNumber, TilskuddFartoyResource tilskuddFartoyResource) throws NotTilskuddfartoyException, GetTilskuddFartoyNotFoundException, GetTilskuddFartoyException, CreateDocumentException, GetDocumentException, IllegalCaseNumberFormat {
+        Case caseByCaseNumber = caseService.getCaseByCaseNumber(caseNumber);
+        if (!isTilskuddFartoy(caseByCaseNumber)) {
+            throw new NotTilskuddfartoyException("Ikke en Tilskuddfartøy sak: " + caseNumber);
+        }
+        for (JournalpostResource journalpostResource : tilskuddFartoyResource.getJournalpost()) {
+            CreateDocumentArgs tilskuddFartoyDocument = tilskuddFartoyFactory.convertToCreateDocument(journalpostResource, caseNumber);
+            documentService.createDocument(tilskuddFartoyDocument);
+        }
+        return getTilskuddFartoyCaseByCaseNumber(caseNumber);
+    }
+
+    public TilskuddFartoyResource getTilskuddFartoyCaseByCaseNumber(String caseNumber) throws NotTilskuddfartoyException, GetTilskuddFartoyNotFoundException, GetTilskuddFartoyException, GetDocumentException, IllegalCaseNumberFormat {
+        Case caseByCaseNumber = caseService.getCaseByCaseNumber(caseNumber);
+
+        if (isTilskuddFartoy(caseByCaseNumber)) {
+            return tilskuddFartoyFactory.toFintResource(caseByCaseNumber);
+        }
+
+        throw new NotTilskuddfartoyException(String.format("MappeId %s er ikke en Tilskuddfartøy sak", caseNumber));
+    }
+
+    public TilskuddFartoyResource getTilskuddFartoyCaseByExternalId(String externalId) throws NotTilskuddfartoyException, GetTilskuddFartoyNotFoundException, GetTilskuddFartoyException, GetDocumentException, IllegalCaseNumberFormat {
+        Case caseResult = caseService.getCaseByExternalId(externalId);
+
+        // TODO Delegate this to tilskuddFartoyFactory
+        if (isTilskuddFartoy(caseResult)) {
+            return tilskuddFartoyFactory.toFintResource(caseResult);
+        }
+
+        throw new NotTilskuddfartoyException("Søknadsnummer " + externalId + " er ikke en Tilskuddfartøy sak");
+    }
+
+    public TilskuddFartoyResource getTilskuddFartoyCaseBySystemId(String systemId) throws NotTilskuddfartoyException, GetTilskuddFartoyNotFoundException, GetTilskuddFartoyException, GetDocumentException, IllegalCaseNumberFormat {
+        Case sakBySystemId = caseService.getCaseBySystemId(systemId);
+
+        // TODO Delegate this to tilskuddFartoyFactory
+        if (isTilskuddFartoy(sakBySystemId)) {
+            return tilskuddFartoyFactory.toFintResource(sakBySystemId);
+        }
+        throw new NotTilskuddfartoyException(String.format("SystemId %s er ikke en Tilskuddfartøy sak", systemId));
+    }
+
+    public List<TilskuddFartoyResource> searchTilskuddFartoyCaseByTitle(Map<String, String> query) throws GetTilskuddFartoyNotFoundException, GetTilskuddFartoyException, GetDocumentException, IllegalCaseNumberFormat {
+
+        String title = String.format("%%%s%%", query.get("title"));
+        String maxReturnedCases = query.getOrDefault("maxResult", "10");
+
+        return tilskuddFartoyFactory.toFintResourceList(
+                caseService.getCasesByTitle(title, maxReturnedCases)
+                        .stream()
+                        .filter(this::isTilskuddFartoy)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    // TODO: 2019-05-11 Should we check for both archive classification and external id (is it a digisak)
+    // TODO Compare with CaseProperties
+    private boolean isTilskuddFartoy(Case caseResult) {
+
+        if (FintUtils.optionalValue(caseResult.getExternalId()).isPresent() && FintUtils.optionalValue(caseResult.getArchiveCodes()).isPresent()) {
+            return caseResult.getExternalId().getType().equals(Constants.EXTERNAL_ID_TYPE);
+        }
+
+        return false;
+
+    }
+
+}
